856568c941e8997b68fffb3be67aac0535d2f542
Karpov Yuri <yurick@bk.ru>
2022-02-17 22:15:07 +0300

    Feat: Добавить методы расчета

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let money = prompt("Ваш бюджет на месяц?", '');
-let time = prompt('Введите дату в формате YYYY-MM-DD', '');
+let money;
+let time;
 
 let appData = {
   budget: money,
@@ -7,13 +7,69 @@ let appData = {
   optionalExpenses: {},
   income: [],
   timeData: time,
-  savings: false
+  savings: false,
+  chooseExpenses: function () {
+    for (let i = 0; i < 2; i++) {
+      let a = prompt("Введите обязательную статью расходов в этом месяце", "");
+      let b = prompt("Во сколько обойдется?", "");
+      if (typeof (a) === 'string' && typeof (a) != null && a != "" && typeof (b) != null && b != "" && a.length < 50) {
+        appData.expenses[a] = b;
+      } else {
+        i--;
+      }
+    }
+  },
+  detectDayBudget: function () {
+    appData.moneyPerDay = (appData.budget / 30).toFixed();
+    alert("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
+  },
+  detectLevel: function () {
+    if (appData.moneyPerDay < 100) {
+      console.log("Это минимальный уровень достатка!");
+    } else if (appData.moneyPerDay >= 100 && appData.moneyPerDay < 2000) {
+      console.log("Это средний уровень достатка!");
+    } else if (appData.moneyPerDay >= 2000) {
+      console.log("Это высокий уровень достатка!");
+    } else {
+      console.log("Ошибочка...!");
+    }
+  },
+  checkSavings: function () {
+    if (appData.savings == true) {
+      let save = +prompt("Какова сумма накоплений?");
+      let percent = +prompt("Под какой процент?");
+      appData.monthIncome = save / 100 / 12 * percent;
+      alert("Доход с Вашего депозита в месяц: " + appData.monthIncome);
+    }
+  },
+  chooseOptExpenses: function () {
+    for (let i = 1; i <= 3; i++) {
+      let questionOptExpenses = prompt("Статья необязательных расходов?");
+      appData.optionalExpenses[i] = questionOptExpenses;
+      console.log(appData.optionalExpenses);
+    }
+  },
+  chooseIncome: function () {
+    let items = prompt("Что принесет дополнительный доход? (Перечислите через запятую)", "");
+    if (typeof (items) != "string" || items == "" || typeof (items) == null) {
+      console.log("Вы ввели некорректные данные или не ввели их вовсе");
+    } else {
+      appData.income = items.split(", ");
+      appData.income.push(prompt("Может что-то еще?"));
+      appData.income.sort();
+    }
+    appData.income.forEach(function (itemmassive, i) {
+      alert("Способы доп. заработка: " + (i + 1) + " - " + itemmassive);
+    });
+  }
 };
 
-for (let i = 0; i < 2; i++) {
-  let a = prompt("Введите обязательную статью расходов в этом месяце", '');
-  let b = prompt("Во сколько обойдется?", '');
-  appData.expenses[a] = b;
+function start() {
+  money = +prompt("Ваш бюджет на месяц?", "");
+  time = prompt("Введите дату в формате YYYY-MM-DD", "");
+  while (isNaN(money) || money == "" || money == null) {
+    money = +prompt("Ваш бюджет на месяц?", "");
+  }
 }
 
-alert(appData.budget / 30);
+start();
