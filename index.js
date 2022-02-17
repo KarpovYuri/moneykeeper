889d472e3867703deed40631b9657c8b33ee170d
Karpov Yuri <yurick@bk.ru>
2022-02-17 14:27:15 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let money = prompt("Ваш бюджет на месяц?", '');
+let time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+let appData = {
+  budget: money,
+  expenses: {},
+  optionalExpenses: {},
+  income: [],
+  timeData: time,
+  savings: false
+};
+
+for (let i = 0; i < 2; i++) {
+  let a = prompt("Введите обязательную статью расходов в этом месяце", '');
+  let b = prompt("Во сколько обойдется?", '');
+  appData.expenses[a] = b;
+}
+
+alert(appData.budget / 30);
